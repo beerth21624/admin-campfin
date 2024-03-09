a7cf76cb2ca83a7ad06c9d2ad062141e222e87a9
beerth21624 <beerzii.4321@gmail.com>
2024-03-09 19:31:45 +0700

    change import key

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import './index.css'
 
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY
+
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}
+  
+    >
     <App />
     </ClerkProvider>
   </React.StrictMode>,
